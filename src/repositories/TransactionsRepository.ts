f91de64690974c25f6573d3a551598c48e3d0db3
Leandro Chaves <leandrochaves.f@gmail.com>
2020-05-23 20:49:00 -0300

    Challenge works fine

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,32 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((total, transaction) => {
+      if (transaction.type === 'income') return total + transaction.value;
+      return total;
+    }, 0);
+    const outcome = this.transactions.reduce((total, transaction) => {
+      if (transaction.type === 'outcome') return total + transaction.value;
+      return total;
+    }, 0);
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,5 +1,22 @@
+/**
+ * STRUCTURE:
+ * 1 - IMPORTS
+ * 2 - TYPING (INTEFACE)
+ * 3 - CLASS
+ * 3.1 - DECLARE
+ * 3.2 - INSTANTIATE
+ * 3.3 - FUNCIONALITIES
+ * 4 - EXPORT CLASS
+ */
+
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
@@ -8,8 +25,31 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    if (typeof value !== 'number') {
+      throw Error('Value must be a number.');
+    }
+
+    if (typeof title !== 'string') {
+      throw Error('Title must be a string.');
+    }
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('Type must be income or outcome.');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && value > total) {
+      throw Error("You don't have enought balance.");
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
